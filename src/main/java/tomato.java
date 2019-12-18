029ac86eb5a9b695c088643f20df6e03f9c431fa
jixw007 <jixiaowei007@126.com>
2019-12-18 16:11:00 +0800

    新增测试

--- src/main/java/tomato.java
+++ src/main/java/tomato.java
@@ -1,5 +1,6 @@
 public class Tomato {
     public static void main(String[] args){
-        System.out.print("hello world !");
+        System.out.print("hello world one !\n");
+        System.out.print("hello world two !\n");
     }
 }
