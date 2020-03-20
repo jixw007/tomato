ef42a265a7ad9f0ec7cb036fec601e29e587636e
jixw007 <jixiaowei007@126.com>
2020-03-20 10:46:55 +0800

    测试

--- src/main/java/MyTest.java
+++ src/main/java/MyTest.java
@@ -39,23 +39,68 @@ public class MyTest {
         System.out.print("hello world !\n");
     }
 
+    //JSON性能测试
+     public static void jsonPerformanceTest(){
+        List<AcctBalanceInfoEntity> acctBalanceInfoEntityList = new ArrayList<AcctBalanceInfoEntity>();
+        List<String> stringList = new ArrayList<String>();
+        for(int i=0; i <100000; i++){
+            AcctBalanceInfoEntity acctBalanceInfoEntity = new AcctBalanceInfoEntity();
+            acctBalanceInfoEntity.setlAcctBalanceID(1111L);
+            acctBalanceInfoEntity.setlBalance(2222L);
+            acctBalanceInfoEntity.setlBalanceTypeId(3333L);
+            acctBalanceInfoEntity.setlNewBalance(4444L);
+            acctBalanceInfoEntity.setsCorpusFlag("6666");
+            acctBalanceInfoEntityList.add(acctBalanceInfoEntity);
+
+            String stringTemp = JSON.toJSONString(acctBalanceInfoEntity);
+            stringList.add(stringTemp);
+        }
+
+
+        long startTime = System.currentTimeMillis();
+        for(AcctBalanceInfoEntity acctBalanceInfoEntity: acctBalanceInfoEntityList){
+            AcctBalanceInfoEntity acctBalanceInfoEntityTemp = new AcctBalanceInfoEntity();
+            acctBalanceInfoEntityTemp.setlAcctBalanceID(acctBalanceInfoEntity.getlAcctBalanceID());
+            acctBalanceInfoEntityTemp.setlBalance(acctBalanceInfoEntity.getlBalance());
+            acctBalanceInfoEntityTemp.setlBalanceTypeId(acctBalanceInfoEntity.getlBalanceTypeId());
+            acctBalanceInfoEntityTemp.setlNewBalance(acctBalanceInfoEntity.getlNewBalance());
+            acctBalanceInfoEntityTemp.setsCorpusFlag(acctBalanceInfoEntity.getsCorpusFlag());
+        }
+        long endTime = System.currentTimeMillis();
+        long costTime = endTime - startTime;
+        System.out.print("acctBalanceInfoEntityList.size()="+acctBalanceInfoEntityList.size()+",costTime="+costTime+"\n");
+
+        long startTime2 = System.currentTimeMillis();
+        for(String stringTemp: stringList){
+            AcctBalanceInfoEntity acctBalanceInfoEntityTemp = new AcctBalanceInfoEntity();
+            acctBalanceInfoEntityTemp = JSON.parseObject(stringTemp, AcctBalanceInfoEntity.class);
+        }
+
+        long endTime2 = System.currentTimeMillis();
+        long costTime2 = endTime2 - startTime2;
+        System.out.print("stringList.size()="+stringList.size()+",costTime2="+costTime2);
+    }
+
     public static void main(String[] args) throws ClassNotFoundException {
 
+        //JSON性能测试
+        jsonPerformanceTest();
+
         //注解测试使用
-        Class clazz = Class.forName("Apple");
-
-        //获取类注解信息
-        AnnotationExample classAnno =(AnnotationExample) clazz.getAnnotation(AnnotationExample.class);
-        System.out.println( classAnno.name()+"---"+classAnno.value()+"---"+classAnno.path());
-
-        //获取所以方法注解信息 ps:这里需要使用 isAnnotationPresent 判断方法上是否使用了注解
-        Method[] allMethods = clazz.getDeclaredMethods();
-        for(int i=0;i<allMethods.length;i++){
-            if(allMethods[i].isAnnotationPresent(AnnotationExample.class)) {
-                AnnotationExample methodAnno = allMethods[i].getAnnotation(AnnotationExample.class);
-                System.out.println("遍历:当前方法名为："+allMethods[i].getName()+" 的注解信息：---"+methodAnno.name() + "---" + methodAnno.value() + "---" + methodAnno.path());
-            }
-        }
+//        Class clazz = Class.forName("Apple");
+//
+//        //获取类注解信息
+//        AnnotationExample classAnno =(AnnotationExample) clazz.getAnnotation(AnnotationExample.class);
+//        System.out.println( classAnno.name()+"---"+classAnno.value()+"---"+classAnno.path());
+//
+//        //获取所以方法注解信息 ps:这里需要使用 isAnnotationPresent 判断方法上是否使用了注解
+//        Method[] allMethods = clazz.getDeclaredMethods();
+//        for(int i=0;i<allMethods.length;i++){
+//            if(allMethods[i].isAnnotationPresent(AnnotationExample.class)) {
+//                AnnotationExample methodAnno = allMethods[i].getAnnotation(AnnotationExample.class);
+//                System.out.println("遍历:当前方法名为："+allMethods[i].getName()+" 的注解信息：---"+methodAnno.name() + "---" + methodAnno.value() + "---" + methodAnno.path());
+//            }
+//        }
 
 
 //        Thread t1 = new Thread(() -> printApples("one"));
@@ -107,46 +152,5 @@ public class MyTest {
 //        System.out.print("filterAppleResult2="+JSON.toJSONString(filterAppleResult2));
 
 
-
- //json转化性能测试
-//        List<AcctBalanceInfoEntity> acctBalanceInfoEntityList = new ArrayList<AcctBalanceInfoEntity>();
-//        List<String> stringList = new ArrayList<String>();
-//        for(int i=0; i <100000; i++){
-//            AcctBalanceInfoEntity acctBalanceInfoEntity = new AcctBalanceInfoEntity();
-//            acctBalanceInfoEntity.setlAcctBalanceID(1111L);
-//            acctBalanceInfoEntity.setlBalance(2222L);
-//            acctBalanceInfoEntity.setlBalanceTypeId(3333L);
-//            acctBalanceInfoEntity.setlNewBalance(4444L);
-//            acctBalanceInfoEntity.setsCorpusFlag("6666");
-//            acctBalanceInfoEntityList.add(acctBalanceInfoEntity);
-//
-//            String stringTemp = JSON.toJSONString(acctBalanceInfoEntity);
-//            stringList.add(stringTemp);
-//        }
-//
-//
-//        long startTime = System.currentTimeMillis();
-//        for(AcctBalanceInfoEntity acctBalanceInfoEntity: acctBalanceInfoEntityList){
-//            AcctBalanceInfoEntity acctBalanceInfoEntityTemp = new AcctBalanceInfoEntity();
-//            acctBalanceInfoEntityTemp.setlAcctBalanceID(acctBalanceInfoEntity.getlAcctBalanceID());
-//            acctBalanceInfoEntityTemp.setlBalance(acctBalanceInfoEntity.getlBalance());
-//            acctBalanceInfoEntityTemp.setlBalanceTypeId(acctBalanceInfoEntity.getlBalanceTypeId());
-//            acctBalanceInfoEntityTemp.setlNewBalance(acctBalanceInfoEntity.getlNewBalance());
-//            acctBalanceInfoEntityTemp.setsCorpusFlag(acctBalanceInfoEntity.getsCorpusFlag());
-//
-//        }
-//        long endTime = System.currentTimeMillis();
-//        long costTime = endTime - startTime;
-//        System.out.print("acctBalanceInfoEntityList.size()="+acctBalanceInfoEntityList.size()+",costTime="+costTime+"\n");
-//
-//        long startTime2 = System.currentTimeMillis();
-//        for(String stringTemp: stringList){
-//            AcctBalanceInfoEntity acctBalanceInfoEntityTemp = new AcctBalanceInfoEntity();
-//            acctBalanceInfoEntityTemp = JSON.parseObject(stringTemp, AcctBalanceInfoEntity.class);
-//        }
-//
-//        long endTime2 = System.currentTimeMillis();
-//        long costTime2 = endTime2 - startTime2;
-//        System.out.print("stringList.size()="+stringList.size()+",costTime2="+costTime2);
     }
 }
