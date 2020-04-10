a09fba0bdbd98901d4ad4a4ee76419624ece33ba
jixw007 <jixiaowei007@126.com>
2020-04-10 17:48:14 +0800

    11

--- src/main/java/com/tomato/mycode/HelloController.java
+++ src/main/java/com/tomato/mycode/HelloController.java
@@ -2,6 +2,7 @@ package com.tomato.mycode;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -9,5 +10,14 @@ public class HelloController {
     public String index(){
         System.out.println("hello my name is HelloController-index,thread_id="+Thread.currentThread().getId()+" ! ");
         return "index";
+    }
+
+    @RequestMapping("/apple")
+    public ModelAndView apple(){
+        System.out.println("hello my name is apple,thread_id="+Thread.currentThread().getId()+" ! ");
+        Apple apple = new Apple();
+        apple.setColor("11");
+        apple.setWeight(22);
+        return new ModelAndView("appleinfo", "apple", apple);
     }
 }
