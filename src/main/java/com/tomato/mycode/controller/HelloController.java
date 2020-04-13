62d070fd2b3af24decfe6de75a09493aa8c2052c
jixw007 <jixiaowei007@126.com>
2020-04-13 16:53:41 +0800

    commit

--- src/main/java/com/tomato/mycode/controller/HelloController.java
+++ src/main/java/com/tomato/mycode/controller/HelloController.java
@@ -1,10 +1,14 @@
 package com.tomato.mycode.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.tomato.mycode.Apple;
 import com.tomato.mycode.dao.AppleDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,11 +27,15 @@ public class HelloController {
     }
 
     @RequestMapping("/get_apple")
-    public ModelAndView getApple(){
+    public ResponseEntity getApple(){
         System.out.println("hello my name is apple,thread_id="+Thread.currentThread().getId()+" ! ");
         Apple apple = appleDao.queryAppleInfo(11L);
 
-        System.out.println("hello my name is apple,apple="+apple+" ! ");
-        return new ModelAndView("appleinfo", "apple", apple);
+        System.out.println("hello my name is apple,apple="+ JSON.toJSONString(apple)+" ! ");
+//        return new ModelAndView("appleinfo", "apple", apple);
+        HttpStatus status = HttpStatus.OK;
+        String body = JSON.toJSONString(apple);
+        HttpHeaders httpHeaders = new HttpHeaders();
+        return new ResponseEntity<>(body, httpHeaders, status);
     }
 }
