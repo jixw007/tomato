77e289dabd29738abd56f325d1898f3468d7064f
jixw007 <jixiaowei007@126.com>
2020-05-12 19:47:04 +0800

    测试提交

---
+++ src/main/java/com/tomato/mycode/MainTest.java
@@ -0,0 +1,8 @@
+package com.tomato.mycode;
+
+public class MainTest {
+    public static void main(String[] args) {
+        int value = 2 << 3;
+        System.out.println("value=" + value);
+    }
+}
