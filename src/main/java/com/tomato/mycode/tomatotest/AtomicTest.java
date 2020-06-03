4547509ba089d9f91bc8052ba1279ba68ffd6e3d
jixw007 <jixiaowei007@126.com>
2020-06-03 09:48:44 +0800

    增加zookpeer客户端写入数据，实现配置

--- src/main/java/com/tomato/mycode/tomatotest/AtomicTest.java
+++ src/main/java/com/tomato/mycode/tomatotest/AtomicTest.java
@@ -6,21 +6,36 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class AtomicTest {
     public static Integer integerCount = 0;
 
+    private static void printinfo(int step) {
+        System.out.printf("ThreadId=%d,ThreadName=%s, step=%d\n", Thread.currentThread().getId(), Thread.currentThread().getName(), step);
+    }
+
     public static void main(String[] args) throws InterruptedException {
         AtomicInteger atomicIntegerCount = new AtomicInteger(0);
 
         System.out.printf("begin  integerCount=%d, atomicInteger=%d\n", integerCount, atomicIntegerCount.get());
 
-        final CountDownLatch latch = new CountDownLatch(100);
+        final CountDownLatch latch = new CountDownLatch(10000);
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             Thread t1 = new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    atomicIntegerCount.incrementAndGet();
-                    integerCount++;
-                    System.out.printf("Thread  integerCount=%d, atomicInteger=%d\n", integerCount, atomicIntegerCount.get());
-                    latch.countDown();
+                    //atomicIntegerCount.incrementAndGet();
+                    //integerCount++;
+
+                    int step = 0;
+                    for (int j = 0; j < 1000; j++) {
+                        step++;
+                        printinfo(step);
+                        //System.out.printf("ThreadId=%d,ThreadName=%s, integerCount=%d, atomicInteger=%d\n", Thread.currentThread().getId(), Thread.currentThread().getName(), integerCount, atomicIntegerCount.get());
+                        latch.countDown();
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
                 }
             });
             t1.start();
